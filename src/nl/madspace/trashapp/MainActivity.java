24da07cd0316abf841df49ad53e74a4b380be04d
Paul Wagener <mail@paulwagener.nl>
2014-02-22 14:24:59 +0100

    Start on Eindhoven

--- src/nl/madspace/trashapp/MainActivity.java
+++ src/nl/madspace/trashapp/MainActivity.java
@@ -3,6 +3,11 @@ package nl.madspace.trashapp;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
 public class MainActivity extends FragmentActivity {
 
 
@@ -10,5 +15,11 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		SupportMapFragment mapFragment = (SupportMapFragment) this
+				.getSupportFragmentManager().findFragmentById(R.id.map);
+		GoogleMap map = mapFragment.getMap();
+
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.4424005, 5.4799979), 12));
 	}
 }
