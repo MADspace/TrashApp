94208b87bb8c518c08e18589436206132ed0ed9b
Paul Wagener <mail@paulwagener.nl>
2014-02-22 19:49:34 +0100

    Added dialog

--- src/nl/madspace/trashapp/MainActivity.java
+++ src/nl/madspace/trashapp/MainActivity.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
@@ -32,7 +36,13 @@ OnInfoWindowClickListener {
 
 		map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.4424005,
 				5.4799979), 12));
-		// map.setOnMarkerClickListener(this);
+
+		Drawable drawable = getResources().getDrawable(R.drawable.vuilniszak);
+		BitmapDrawable bd = (BitmapDrawable)drawable.getCurrent();
+		Bitmap b = bd.getBitmap();
+		Bitmap bhalfsize=Bitmap.createScaledBitmap(b, b.getWidth()/2,b.getHeight()/2, false);
+
+
 		map.setOnInfoWindowClickListener(this);
 		InputStream is = getResources().openRawResource(R.raw.trashcans);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
@@ -44,7 +54,7 @@ OnInfoWindowClickListener {
 				double lng = Double.parseDouble(rowdata[1]);
 				map.addMarker(new MarkerOptions()
 				.position(new LatLng(lat, lng)).title(
-						"Klaag over deze afvalbak").icon(BitmapDescriptorFactory.fromResource(R.drawable.vuilniszak)));
+						"Klaag over deze afvalbak").icon(BitmapDescriptorFactory.fromBitmap(bhalfsize)));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -56,5 +66,16 @@ OnInfoWindowClickListener {
 	public void onInfoWindowClick(Marker marker) {
 		Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		startActivityForResult(takePicture, 0);
+	}
+
+	@Override
+	protected void onActivityResult(int arg0, int arg1, Intent arg2) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(arg0, arg1, arg2);
+		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+		alertDialog.setTitle("Succes");
+		alertDialog.setMessage("Uw klacht en foto is naar de gemeente verstuurd!");
+
+		alertDialog.show();
 	}
 }
